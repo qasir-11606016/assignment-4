8d23259e380837ef6f87de972437210ef7a1eec2
qasir-11606016 <aliqasier@hotmail.com>
2016-10-09 15:42:36 +1100

    creating test for the limit

---
+++ TC5.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TC5 {
+	 Dice d1 = new Dice();
+     Dice d2 = new Dice();
+     Dice d3 = new Dice();
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
