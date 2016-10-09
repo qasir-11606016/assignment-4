507e15942c3338fb8dd56f3ad75333befa06b26b
qasir-11606016 <aliqasier@hotmail.com>
2016-10-09 14:47:00 +1100

    find the error

--- TC4.java
+++ TC4.java
@@ -4,21 +4,21 @@ import org.junit.Test;
 
 public class TC4 {
 
-	Player player =new Player("Fred",4);
+	Player player =new Player("Fred",5);
     int bet= 5;
     int limit =0;
     int winning=5;
     @Test
 	public void test() {
 	int actual=	player.getBalance();
-	int expected=3;
+	int expected=5;
 	assertEquals(actual,expected);
 	}
 	@Test
 	public void test8() {
 		player.receiveWinnings(winning);
 	int actual =player.getBalance();
-	int expected =4+bet;
+	int expected =5+bet;
 	assertEquals(actual,expected);
 	}
 	public void test6() {
