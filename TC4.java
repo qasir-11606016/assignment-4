191a16f5d273dfc5f41ddab7ddaa315e64c7f8da
qasir-11606016 <aliqasier@hotmail.com>
2016-10-09 14:34:13 +1100

    with new data set

---
+++ TC4.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TC4 {
+
+	Player player =new Player("Fred",2);
+    int bet= 5;
+    int limit =0;
+    int winning=5;
+
+}
