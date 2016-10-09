95073d3d95328de45d8de08b580eddaba21719e1
qasir-11606016 <aliqasier@hotmail.com>
2016-10-09 15:43:49 +1100

    intial setup for the tes t

--- TC5.java
+++ TC5.java
@@ -6,6 +6,11 @@ public class TC5 {
 	 Dice d1 = new Dice();
      Dice d2 = new Dice();
      Dice d3 = new Dice();
+     Player player =new Player("Fred",100);
+     Game game = new Game(d1, d2, d3);
+     int bet= 5;
+     int limit =0;
+     int winning=5;
 	@Test
 	public void test() {
 		fail("Not yet implemented");
