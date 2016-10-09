a557a4410176579a4ae3d7e5c759dd1534f35d43
qasir-11606016 <aliqasier@hotmail.com>
2016-10-09 13:42:56 +1100

    creating a new test for balance value change

---
+++ TC2.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TC2 {
+	public class TC1 {
+		 Dice d1 = new Dice();
+	     Dice d2 = new Dice();
+	     Dice d3 = new Dice();
+	     Player player =new Player("Fred",0);
+	     Game game = new Game(d1, d2, d3);
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
