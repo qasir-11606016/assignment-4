8b4c2651f7430198c12d1e7e1933166eaede717c
qasir-11606016 <aliqasier@hotmail.com>
2016-10-09 14:07:17 +1100

    changing code

--- Player.java
+++ Player.java
@@ -38,7 +38,7 @@ public class Player {
 	
 	public void receiveWinnings(int winnings) {
 		if (winnings < 0) throw new IllegalArgumentException("Winnings cannot be negative.");
-		balance = balance + (winnings+5);		
+		balance = balance + (winnings);		
 	}
 	
 	public String toString() {
