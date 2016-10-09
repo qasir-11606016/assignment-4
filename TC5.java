d20af6495ddaedab077064e8cf194406b59979c8
qasir-11606016 <aliqasier@hotmail.com>
2016-10-09 15:49:45 +1100

    test for get balance

--- TC5.java
+++ TC5.java
@@ -6,7 +6,7 @@ public class TC5 {
 	 Dice d1 = new Dice();
      Dice d2 = new Dice();
      Dice d3 = new Dice();
-     Player player =new Player("Fred",100);
+     Player player =new Player("Fred",5);
      Game game = new Game(d1, d2, d3);
      int bet= 5;
      int limit =0;
@@ -27,6 +27,12 @@ public class TC5 {
  	public void test3() {
  	int actual=	player.getLimit();
  	int expected=0;
+ 	assertEquals(actual,expected);
+ 	}
+     @Test
+ 	public void test4() {
+ 	int actual=	player.getBalance();
+ 	int expected=5;
  	assertEquals(actual,expected);
  	}
 
